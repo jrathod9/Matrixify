4680887e4530ec4806c9f084c11216b31d5efe18
jrathod9 <lewisburnard@gmail.com>
2019-04-21 21:44:33 +0530

    File upload fixed

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template , url_for ,flash, redirect, request
+import os
+from werkzeug import secure_filename
+
 app = Flask(__name__)
 from PIL import Image
 
 app.config['SECRET_KEY']='151de16d04dacf29c88db46b69194afb'
+app.config['UPLOAD_FOLDER']=''
 
 SYMBOLS = ['.',',',':',';','1','2','3','4','5','6','@']
 resize_width = 50
@@ -31,13 +35,14 @@ def home():
 @app.route("/result",methods = ['GET','POST'])
 def result():
 	if request.method == 'POST':
-		image = request.form["file"]
+		image = request.files["file"]
+		this_file = image
+		image.save(secure_filename(image.filename))
 		if image :
-			name_file = image.split(".")
+			name_file = image.filename.split(".")
 			name_file = name_file[1]
 		file_types = ['png','jpg','jpeg','gif','bmp','tiff']
 		if image and name_file in file_types:
-			image = 'static/' + image
 			im = Image.open(image,'r')
 			im = resize(im)
 			clr=[]
@@ -46,7 +51,6 @@ def result():
 				clr.append(ele)
 			im = im.convert('L')
 			im = symbolizer(im)
-
 			count = 0
 			length = len(im)
 			new_image = [im[index:index+resize_width] for index in range(0, length, resize_width)]
@@ -55,6 +59,7 @@ def result():
 					pixel = [pixel,clr[0],clr[1],clr[2],clr[3]]
 					print(pixel[0],end="")
 				print("")
+			os.remove(os.path.join(app.config['UPLOAD_FOLDER'],this_file.filename))
 		else:
 			new_image = []
 	return render_template('result.html',image = new_image)
